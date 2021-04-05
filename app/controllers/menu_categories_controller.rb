d991b417b2cbc915e2c0e8788b948d23f2f0e395
Rajanayaki <rajanayaki4444@gmail.com>
2021-04-05 23:28:39 +0530

    Devise pages are set

--- app/controllers/menu_categories_controller.rb
+++ app/controllers/menu_categories_controller.rb
@@ -1,6 +1,18 @@
 class MenuCategoriesController < ApplicationController
     def index
-        render plain: MenuCategory.all.to_a
+        render "index"
+    end
+
+    def create
+        menu_category_name = params[:menu_category_name]
+        new_menu_category= MenuCategory.new(
+            name: menu_category_name
+        )
+        if new_menu_category.save
+            redirect_to menu_categories_path
+        else
+            redirect_to "/"
+        end
     end
 
 end
---
+++ app/models/consumer.rb
@@ -0,0 +1,4 @@
+class Consumer < ActiveRecord::Base
+    has_many :orders
+
+end

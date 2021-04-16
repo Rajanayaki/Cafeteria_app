b5f510726f25dd443bb22d9b7c637007853fe6fc
Rajanayaki <rajanayaki4444@gmail.com>
2021-04-17 02:27:47 +0530

    Reset password links done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,14 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+ActionMailer::Base.smtp_settings = {  
+  :address              => "smtp.gmail.com",  
+  :port                 => 587,  
+  :domain               => "gmail.com",  
+  :user_name            => "noreplydemo20@gmail.com",  
+  :password             => "Happiebird@20",  
+  :authentication       => "plain"
+  # :enable_starttls_auto => true # I don't have this, but it should work anyway 
+} 
 end

d3a003f3ac3cd155f2df2a29ed10f71f474765cd
Rajanayaki <rajanayaki4444@gmail.com>
2021-04-21 22:48:33 +0530

    Restricting access

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,6 +1,11 @@
 class OrderItemsController < ApplicationController
     def index
         @order_id = params[:order_id]
-        render "index" , locals: { order_id: @order_id }
+        order=Order.find(@order_id)
+        if current_user.id == order.user_id
+            render "index"
+        else
+            redirect_to "/"
+        end        
     end
 end 
